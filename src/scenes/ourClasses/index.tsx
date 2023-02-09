29399a6c406c1a9aa62677ddc1ca7b6919444be9
Alec_Shaw <oleg.iliushov@gmail.com>
2023-02-09 11:50:49 +0300

    more new link

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -13,6 +13,12 @@ const classes: Array<ClassType> = [
         image: "https://www.youtube.com/embed/UejvgFSJyEw",
     },
     {
+        name: "Weight Training Classes",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        image: "https://www.youtube.com/embed/3cXwpyhA9oQ",
+    },
+    {
         name: "Yoga Classes",
         image: "https://www.youtube.com/embed/xTb3H3thGU4",
     },
